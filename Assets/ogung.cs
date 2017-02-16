546df1dce968e7d50dc20bae0e05f4bb1eecd98f
ricegood <951227@gmail.com>
2017-02-16 22:24:28 +0900

    Play Scene, Jump, Move

---
+++ Assets/ogung.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ogung : MonoBehaviour {
+
+	public int speed;
+	public bool lookRight;
+
+	private SpriteRenderer spr;
+	private Transform trs;
+
+	private Vector3 movePos = new Vector3(0.03f , 0f , 0f);
+	private Vector3 jumpPos = new Vector3(0f , 0.05f , 0f);
+	private Vector3 scaleY = new Vector3(0f, 0.1f, 0f);
+
+	// Use this for initialization
+	void Start () {
+		spr = GetComponent<SpriteRenderer> ();
+		trs = GetComponent<Transform> ();
+	}
+
+	void FixedUpdate()
+	{
+		if(Input.GetKey("left")){
+			spr.flipX = true;
+			trs.position -= speed*movePos;
+		}
+
+		if(Input.GetKey("right")){
+			spr.flipX = false;
+			trs.position += speed*movePos;
+		}
+
+		if(Input.GetKey(".")){
+			trs.position += speed*jumpPos;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
